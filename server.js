afacc6d1904dc73e40454105feaa0753ec99994c
JC Reid <thawedbuffalo@gmail.com>
2021-01-22 13:11:41 -0500

    port updates

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = 'localhost';
 
 // App
 const app = express();
--- web-hooks/redeploy.sh
+++ web-hooks/redeploy.sh
@@ -3,4 +3,4 @@ echo 'executing hook...'
 docker pull thawedbuffalosolutions/docker-node-app:latest
 docker stop docker-node-app
 docker system prune -f
-docker run -d --name=docker-node-app thawedbuffalosolutions/docker-node-app:latest
+docker run -p 3000:3000 -d --name=docker-node-app thawedbuffalosolutions/docker-node-app:latest
